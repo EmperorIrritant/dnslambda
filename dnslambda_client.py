f926f9a5d9c1e1de3e4b3517938d7ab83020f161
EmperorIrritant <95113377+EmperorIrritant@users.noreply.github.com>
2022-01-03 18:43:07 +0000

    Implemented dummy responses to DNS queries in client
    - Most code copied from dnslib.server.DNSHandler
    - Added dummy response to all replies

--- dnslambda_client.py
+++ dnslambda_client.py
@@ -1,17 +1,61 @@
 import dnslib.server
+from dnslib.dns import DNSRecord, RR, DNSError
 import argparse, sys, time
+import socket
+import struct
+import socketserver
 
-class DNSRequestHandler(dnslib.server.DNSHandler):
+class DNSRequestHandler(socketserver.BaseRequestHandler):
+
+    udplen = 0
+
     def handle(self):
         # Receive data from request
+        if self.server.socket_type == socket.SOCK_STREAM:
+            self.protocol = 'tcp'
+            data = self.request.recv(8192)
+            if len(data) < 2:
+                return
+            length = struct.unpack("!H", bytes(data[:2]))[0]
+            while len(data) - 2 < length:
+                new_data = self.request.recv(8192)
+                if not new_data:
+                    break
+                data += new_data
+            data = data[2:]
+        else:
+            self.protocol = 'udp'
+            data, connection = self.request
+
         # Parse data to DNSRecord
+        request = dnslib.server.DNSRecord.parse(data)
         # Extract query information from DNSRecord
-        # call get_reply() with query information
-        # make DNSRecord from get_reply response
-        # pack DNSRecord object with dnslib
-        # pack with struct.pack
-        # send reply back to the client
-        pass
+        qname, qtype = str(request.q.qname), request.q.qtype
+        try:
+            # call get_reply() with query information
+            reply = self.get_reply((qname, qtype))
+
+            # make DNSRecord from get_reply response
+            response = request.reply()
+            response.add_answer(*RR.fromZone("abc.com A 1.2.3.4"))
+
+            # pack DNSRecord object with dnslib
+            if self.protocol == 'udp':
+                rdata = response.pack()
+                if self.udplen and len(rdata) > self.udplen:
+                    truncated_reply = reply.truncate()
+                    rdata = truncated_reply.pack()
+            else:
+                rdata = reply.pack()
+            # pack with struct.pack
+            # send reply back to the client
+            if self.protocol == 'tcp':
+                rdata = struct.pack("!H", len(rdata)) + rdata
+                self.request.sendall(rdata)
+            else:
+                connection.sendto(rdata, self.client_address)
+        except DNSError as e:
+            print(e)
 
     def get_reply(self, data):
         # call Lambda with AWS user credentials (Later - call using a Role given by Cognito)
